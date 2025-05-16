4814704cafef7186e3f1fd9fe7e537640bfffe1a
vincent44-art <vincent.murithi@student.moringaschoo.com>
2025-05-16 20:36:23 +0300

    commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        word_sorted = sorted(self.word.lower())
+        return [
+            candidate for candidate in possible_anagrams
+            if sorted(candidate.lower()) == word_sorted and candidate.lower() != self.word.lower()
+        ]
